c271598341bfc24eb005f2205fada0b26964f78f
Juha Tauriainen <juha@bin.fi>
2025-09-01 14:04:37 +0300

    Add stupid comment

--- src/GreetingMachine.java
+++ src/GreetingMachine.java
@@ -7,5 +7,7 @@ public class GreetingMachine {
     public static void main(String[] args) {
         GreetingMachine gm = new GreetingMachine();
         gm.greet();
+
+        // no onpa kiva homma
     }
 }
